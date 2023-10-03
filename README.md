e02458c6e9aea693afe3e09e4e284e6fae73b882
alonscheuer <11799711+alonscheuer@users.noreply.github.com>
2023-10-03 19:40:50 +0200

    added comments and documentation

--- README.md
+++ README.md
@@ -1,3 +1,65 @@
 # Task Baseline System
 
-Note that the data is not uploaded to this repository. You may find the data at [mbzuai0nlp/SemEval2024-task8](https://github.com/mbzuai-nlp/SemEval2024-task8).
+Note that the data itself is not provided here. You may find it at [mbzuai0nlp/SemEval2024-task8](https://github.com/mbzuai-nlp/SemEval2024-task8).
+
+## Feature-based Baseline
+
+### Setup
+
+The code requires the <b>Scikit-learn</b> package. To install it run:
+```
+pip install scikit-learn
+```
+
+### Usage
+
+```
+python feature_baseline.py [options]
+```
+
+<b>Note:</b> the code assumes that the data is structured like the official data-set, and is located in `./data/`:
+```
+- project
+  |- feature_baseline.py
+  |- data
+     |- SubtaskA...
+     |- SubtaskB...
+     |- SubtaskC...
+```
+
+#### Arguments
+
+| Argument | Description |
+| --- | --- |
+| `-t`<br/>`--task` | Specify which task to run <br/> Values: `a_mono, a_multi, b, c` |
+| `-m`<br/>`--model` | Specify which model to use <br/> Values: `NB, DT, RF, KNN, SVM, SGD`* <br/> * only use for task C in  regression mode |
+| `-cm`<br/>`--c_method` | Specify which method to use for task C <br/> Values: `regression, split` <br/> <b>`regression`</b> method uses a regression model specified by `-m` trained on subtask C data. <b>`split`</b> method uses a classifier model specified by `-m` trained on subtask A (monolingual) data and employing a split-method specified by `-sm`.|
+| `-sm`<br/>`--split_method` | Specify which split-method to use for task C in split mode <br/> Values: `first, max, binary` <br/> <b>`first`</b> picks the index of the first token which is classified as machine-generated. <b>`max`</b> scores all possible splits and selects the best one. <b>`binary`</b> uses binary search to find the point of change. |
+| `-f`<br/>`--fraction` | Specify what fraction of the data to use <br/> Default is 1.0 (=100%)
+
+
+
+#### Examples
+
+Subtask A monolingual using a Decision Tree classifier:
+```
+python feature_baseline.py -t a_mono -m DT
+```
+Subtask A multilingual using a Random Forest classifier:
+```
+python feature_baseline.py -t a_multi -m RF
+```
+
+Subtask B using a Random Forest classifier:
+```
+python feature_baseline.py -t b -m RF
+```
+
+Subtask C using a SGD regression model:
+```
+python feature_baseline.py -t c -cm regression -m SGD
+```
+Subtask C using a Decision Tree model + binary split:
+```
+python feature_baseline.py -t c -cm split -m DT -sm binary
+```
--- feature_baseline.py
+++ feature_baseline.py
@@ -14,25 +14,25 @@ from statistics import mean
 def create_arg_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--task", type=str, help="Which task to evaluate (a_mono, a_multi, b, c)")
-    parser.add_argument("-m", "--model", type=str, help="Which model to use (NB, RF, SVC, DT, KNN)")
-    parser.add_argument("-a", "--all", action="store_true", help="Run all models on all tasks")
-    parser.add_argument("-f", "--fraction", type=float, default=0.05, help="Fraction of the data to use (default 0.05)")
-    parser.add_argument("-sm", "--split_method", type=str, help="Which split method to use (first, max_split, binary)")
+    parser.add_argument("-m", "--model", type=str, help="Which model to use")
+    parser.add_argument("-f", "--fraction", type=float, default=1.0, help="Fraction of the data to use (default 1.0 = 100%)")
+    parser.add_argument("-cm", "--c_method", type=str, default="split", help="Method to use for subtask C (regression, split)")
+    parser.add_argument("-sm", "--split_method", type=str, help="Which split method to use (first, max, binary)")
     return parser.parse_args()
 
-labels_a = {
-    0: "human",
-    1: "machine"
-}
+# labels_a = {
+#     0: "human",
+#     1: "machine"
+# }
 
-labels_b = {
-    0: "human",
-    1: "ChatGPT",
-    2: "Cohere",
-    3: "Davinci",
-    4: "Bloomz",
-    5: "Dolly"
-}
+# labels_b = {
+#     0: "human",
+#     1: "ChatGPT",
+#     2: "Cohere",
+#     3: "Davinci",
+#     4: "Bloomz",
+#     5: "Dolly"
+# }
 
 tasks = {
     "a_mono": {
@@ -48,53 +48,59 @@ tasks = {
         "dev_data": "data/SubtaskB/subtaskB_dev.jsonl"
     },
     "c": {
-        "train_data": "data/SubtaskA/subtaskA_train_monolingual.jsonl",
+        "train_data": "data/SubtaskC/subtaskC_train.jsonl",
         "dev_data": "data/SubtaskC/subtaskC_dev.jsonl"
     }
 }
 
 models = {
+    # Classification models
     "NB": MultinomialNB(),
     "RF": RandomForestClassifier(),
     "SVM": SVC(kernel="sigmoid"),
     "DT": DecisionTreeClassifier(),
-    "KNN": KNeighborsClassifier(n_neighbors=3)
-}
-
-regression_models = {
+    "KNN": KNeighborsClassifier(n_neighbors=3),
+    # Regression models
     "SGD": SGDRegressor(max_iter=10000, verbose=1),
     "LR": LinearRegression(),
     "LSVR": LinearSVR(),
     "SVR": SVR(kernel="sigmoid")
 }
 
+# find splitting point using binary search
 def binary_split(array):
     start = 0
     end = len(array) - 1
     mid = int((start + end) / 2)
     while (start < mid):
-        before = mean(array[start:mid])
-        after = mean(array[mid:end])
-        bias_before = abs(before - 0)
-        bias_after = abs(1 - after)
-        if bias_before > bias_after:
+        # first half must be closest to human (0)
+        score_first = 1 - mean(array[start:mid])
+        # second half must be closest to machine (1)
+        score_second = mean(array[mid:end])
+        if score_first < score_second:
+            # first half is further away from optimal,
+            # so it might still contain mixed text
             end = mid - 1
             mid = int((start + end) / 2)
         else:
+            # second half is further away from optimal,
+            # so it might still contain mixed text
             start = mid
             mid = int((start + end) / 2)
     return mid
 
-# find most optimal splitting point
+# find most optimal splitting point among all possible points
 def max_split(tags):
+    # score each split by sum of closeness of each part to its optimal value
+    # (0 for first part, 1 for second part)
     scores = [1 - mean(tags[:i]) + mean(tags[i:]) for i in range(1, len(tags))]
     split_index = scores.index(max(scores))
     return split_index
 
+# use split method to predict boundary of a single text
 def find_split(model, data, method):
     tokens = data.split(" ")
     labels = model.predict(tokens).tolist()
-    # find split index
     if method == "first":
         split_index = labels.index(1)
     elif method == "max":
@@ -103,14 +109,15 @@ def find_split(model, data, method):
         split_index = binary_split(labels)
     return split_index
 
-def task_c_predict(model, data, method):
+# process data text-by-text and predict boundaries
+def split_predict(model, data, method):
     y = []
     for sample in data:
         y.append(find_split(model, sample, method))
     return y
 
-def run_pipeline(task, model, fraction, split_method):
-    print(f"--- Now running task {task} with model {model} on {int(fraction*100)}% of the data {f'using {split_method}-split as the split method' if task == 'c' else ''}---")
+def run_pipeline(task, model, fraction):
+    print(f"--- Now running task {task} using a {model} and {int(fraction*100)}% of the data ---")
 
     train_data = pandas.read_json(tasks[task]["train_data"], lines=True)
     dev_data = pandas.read_json(tasks[task]["dev_data"], lines=True)
@@ -127,24 +134,43 @@ def run_pipeline(task, model, fraction, split_method):
 
     pipe = make_pipeline(TfidfVectorizer(ngram_range=(1,2), min_df=0.02, max_df=0.8), clf)
     pipe.fit(X_train, y_train)
+
+    y_pred = pipe.predict(X_dev)
     
     if task == "c":
-        y_pred = task_c_predict(pipe, X_dev, split_method)
         print(mean_absolute_error(y_dev, y_pred))
     else:
-        y_pred = pipe.predict(X_dev)
         print(classification_report(y_dev, y_pred))
         print(confusion_matrix(y_dev, y_pred))
 
+def run_pipeline_split(model, fraction, split_method):
+    print(f"--- Now running task c using a {model} + {split_method}-split model and {int(fraction*100)}% of the data ---")
+    
+    train_data = pandas.read_json(tasks["a_mono"]["train_data"], lines=True)
+    dev_data = pandas.read_json(tasks["c"]["dev_data"], lines=True)
+
+    train_data = train_data.sample(frac=fraction)
+    dev_data = dev_data.sample(frac=fraction)
+
+    X_train = train_data["text"]
+    y_train = train_data["label"]
+    X_dev = dev_data["text"]
+    y_dev = dev_data["label"]        
+    
+    clf = models[model]
+
+    pipe = make_pipeline(TfidfVectorizer(ngram_range=(1,2), min_df=0.02, max_df=0.8), clf)
+    pipe.fit(X_train, y_train)
+
+    y_pred = split_predict(pipe, X_dev, split_method)
+    print(mean_absolute_error(y_dev, y_pred))
+
 def main():
     args = create_arg_parser()
-    if args.all:
-        for task in tasks.keys():
-            for model in models.keys():
-                run_pipeline(task, model, 1.0)
+    if args.task == "c" and args.c_method == "split":
+        run_pipeline_split(args.model, args.fraction, args.split_method)
     else:
-        run_pipeline(args.task, args.model, args.fraction, args.split_method)
-    
+        run_pipeline(args.task, args.model, args.fraction)
 
 if __name__ == "__main__":
      main()
